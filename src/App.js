71fba9e99be500c6c15de232904d2d966f3b68a5
refaldy04 <refaldybanggana@gmail.com>
2022-10-12 16:38:35 +0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
+import Navbar from './components/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <Row>
+          <Col className="d-flex flex-column justify-content-center align-items-center">
+            <h1 className="tagline">Kirim berbagai jenis ikan dengan mudah</h1>
+            <p className="text-secondary">A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's </p>
+            <div className="d-flex gap-3 justify-content-start w-100">
+              <img src="./google-play.svg" alt="google" className="button" />
+              <img src="./apple-store.svg" alt="apple" className="button" />
+            </div>
+            <div className="d-flex justify-content-start w-100 gap-4">
+              <div>
+                <h1>1.9k+</h1>
+                <p>Active Users</p>
+              </div>
+              <div>
+                <h1>20++</h1>
+                <p>New Features</p>
+              </div>
+            </div>
+          </Col>
+          {/* <Col>
+            <img src="./fish.png" className="fish" alt="fish" />
+          </Col> */}
+          <Col className="d-flex flex-column justify-content-center align-items-center">
+            <img src="./test.png" className="phone" alt="phone" />
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
