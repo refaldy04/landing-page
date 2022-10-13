e3afaf81379387cbd6f358802d9712c494f110a6
refaldy04 <refaldybanggana@gmail.com>
2022-10-14 05:50:58 +0700

    fix display

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Navbar from './components/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
@@ -79,25 +79,25 @@ function App() {
       </Container>
       <Container>
         <Row>
-          <Col xs="12" className="d-flex flex-column justify-content-center align-items-center mt-5">
+          <Col xs="12" className="d-flex flex-column justify-content-center align-items-center mt-5 mb-5">
             <h1 className="fw-bold">Keunggulan kami</h1>
             <p className="text-center col-6 mt-3">A Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
           </Col>
         </Row>
       </Container>
       <Container>
-        <Row className="frame-67 flex-row-vcenter-hstart">
-          <Col xs="12" md="4" className="services flex-col-hcenter-vcenter">
+        <Row className="mt-5">
+          <Col xs="12" md="4" className="d-flex flex-column align-items-center justify-content-center">
             <img src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1xut85eztd5-1%3A51?alt=media&token=c86760b2-cd22-46df-b620-1df90535e863" alt="Not Found" className="component-1" />
             <p className="txt-175">Kirim Ke Berbagai Negara</p>
             <p className="txt-747 flex-hcenter">A Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
           </Col>
-          <Col xs="12" md="4" className="services-1 flex-col-hcenter-vcenter">
+          <Col xs="12" md="4" className="d-flex flex-column align-items-center justify-content-center">
             <img src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1xut85eztd5-1%3A58?alt=media&token=8b85732c-5553-4ffe-a9af-88deaab4d047" alt="Not Found" className="component-1" />
             <p className="txt-175">Mudah Di Gunakan</p>
             <p className="txt-747 flex-hcenter">A Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
           </Col>
-          <Col xs="12" md="4" className="services-2 flex-col-hcenter-vcenter">
+          <Col xs="12" md="4" className="d-flex flex-column align-items-center justify-content-center">
             <img src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1xut85eztd5-1%3A66?alt=media&token=f7e98bc9-e6b4-49e1-81db-37fab832d688" alt="Not Found" className="component-1" />
             <p className="txt-172">Pengiriman Aman</p>
             <p className="txt-747 flex-hcenter">A Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
